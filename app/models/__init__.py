e10221fa72f4a7453a0e1389e2cdd41b7350930f
dplvv <polivaev04@gmail.com>
2026-05-14 18:12:25 +0300

    data: добавлен пакет моделей

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.notification import Notification
+from app.models.page_monitor import PageMonitor
+from app.models.screenshot import Screenshot
+from app.models.visual_diff import VisualDiff
+
+__all__ = ["PageMonitor", "Screenshot", "VisualDiff", "Notification"]
